2ec329a617b412849bf0c05544e977d3ca7cef13
Joseph Rouff <rouffj@gmail.com>
2013-05-26 19:29:10 +0200

    Finish Howto

--- Controller/Security/Voter/RequestController.php
+++ Controller/Security/Voter/RequestController.php
@@ -1,30 +1,30 @@
 <?php
 
-namespace Rouffj\Bundle\HowToBundle\Controller\Security;
+namespace Rouffj\Bundle\HowToBundle\Controller\Security\Voter;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
 /**
- * @Route("/voter")
+ * @Route("/voter/request")
  */
-class VoterController extends Controller
+class RequestController extends Controller
 {
     /**
-     * @Route("/1", name="security_voter_beta_homepage")
+     * @Route("", name="security_voter_beta_homepage")
      * @Template()
      */
-    public function firstAction()
+    public function homepageAction()
     {
         return array();
     }
 
     /**
-     * @Route("/1/app", name="security_voter_beta_app")
+     * @Route("/app", name="security_voter_beta_app")
      * @Template()
      */
-    public function secAction()
+    public function appAction()
     {
         return array();
     }
--- Tests/Symfony/Security/VoterTest.php
+++ Tests/Symfony/Security/VoterTest.php
@@ -4,24 +4,24 @@ namespace Rouffj\Bundle\HowToBundle\Tests\Symfony\Security;
 
 use Rouffj\Bundle\HowToBundle\Tests\WebTestCase;
 
-class AuthorizationTest extends WebTestCase
+class VoterTest extends WebTestCase
 {
     public function testHowToUseVoterForRequestBasedAuthorization()
     {
         // case 1
-        $crawler = $this->client->request('GET', '/howto/security/voter/1');
+        $crawler = $this->client->request('GET', '/howto/security/voter/request');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode(), 'The homepage should be accessible for non signin users');
 
-        $this->client->signin('user1', 'user1pass');
+        $this->client->signin('user1');
         $this->client->openLink('Join the private beta', $crawler);
 
         $this->assertEquals('user1', $this->client->getContainer()->get('security.context')->getToken()->getUser()->getUsername(), 'The user should be connected');
         $this->assertEquals(403, $this->client->getResponse()->getStatusCode(), 'But should have access denied because he is not in "authorized login" list given to BetaVoter');
 
         // case 2
-        $crawler = $this->client->request('GET', '/howto/security/voter/1');
+        $crawler = $this->client->request('GET', '/howto/security/voter/request');
 
-        $this->client->signin('user2', 'user2pass');
+        $this->client->signin('user2');
         $this->client->openLink('Join the private beta', $crawler);
 
         $this->assertEquals('user2', $this->client->getContainer()->get('security.context')->getToken()->getUser()->getUsername(), 'The user should be connected');
