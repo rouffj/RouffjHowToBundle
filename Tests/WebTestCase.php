d141673f30cafa1a6f6c9d9681a08305b5db3907
Joseph Rouff <rouffj@gmail.com>
2013-09-01 00:08:00 +0200

    Add setup database

--- Tests/WebTestCase.php
+++ Tests/WebTestCase.php
@@ -3,6 +3,8 @@
 namespace Rouffj\Bundle\HowToBundle\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Bundle\FrameworkBundle\Console\Application;
 
 abstract class WebTestCase extends BaseWebTestCase
 {
@@ -13,5 +15,25 @@ abstract class WebTestCase extends BaseWebTestCase
         parent::setUp();
 
         $this->client = self::createClient();
+
+        $application = new Application(static::$kernel);
+        $application->setAutoExit(false);
+        $application->run(new ArrayInput(array(
+            'doctrine:schema:drop',
+            '--force' => true,
+            '--full-database' => true,
+            '--no-debug' => true,
+            '--quiet'    => true,
+        )));
+        $application->run(new ArrayInput(array(
+            'doctrine:schema:create',
+            '--no-debug' => true,
+            '--quiet'    => true,
+        )));
+        $application->run(new ArrayInput(array(
+            'doctrine:fixtures:load',
+            '--no-debug' => true,
+            '--quiet'    => true,
+        )));
     }
 }
